3abc4890a51ca62908575178ed94e0cc0b50f9f0
Anton Blomberg <anton@naetet.se>
2014-07-04 00:55:00 +0200

    Added expandable view on Area, multiple topos and fixed lots of small stuff

--- 27proxy.py
+++ 27proxy.py
@@ -26,49 +26,128 @@ class Handler(BaseHTTPRequestHandler):
                 with open('responsive.css') as f:
                     responsive_css = f.read()
             content = r.content.replace(
-                '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js">',
+                '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>',
                 '<meta name="viewport" content="width=device-width">\n' +
                 '<style type="text/css">\n%s\n</style>\n' % responsive_css +
-                '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js">')
+                '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>\n' +
+                """
+                <script type="text/javascript">
+                    $(document).ready(function() {
+                        $("#map").html('<a href="http://27crags.com%s">Map on original site</a>');
+                        $("#map").show();
+                        $("#map").attr("id", "no-map");
+                    });
+                </script>
+                """ % self.path)
 
             content = content.replace(
                 '</head>', """
                 <script type="text/javascript">
-                    $(document).ready(function() {
-                        var canvas = $(".topobox canvas");
-                        var canvas_width = canvas.width();
-                        setTimeout(function() {
-                        $("head").append(" \
-                            <style type='text/css'> \
-                                .pics .topobox { \
-                                    width: 100% !important; \
-                                } \
-                                \
-                                .pics .topobox { \
-                                    height: auto !important; \
-                                } \
-                                \
-                                .topobox img { \
-                                    width: 100%; \
-                                    height: auto; \
-                                } \
-                                \
-                                .topobox canvas { \
-                                    width: 100%; \
-                                } \
-                            </style> \
-                        ");
+                    var addCell = function(to, from, num, caption, bar) {
+                        console.log(from.find("td:nth-child(" + num + ")"));
+                        to.append('<span>' + caption + '</span> ' +
+                                  from.find("td:nth-child(" + num + ")").text() + '<span>' + bar + '</span>');
+                    };
 
-                        var new_canvas_width = canvas.width();
-                        var ratio = new_canvas_width / canvas_width;
-                        $(".topobox .nbr").each(function() {
-                            var jq_elem = $(this);
-                            var left = parseInt(jq_elem.css('left'), 10);
-                            jq_elem.css("left", Math.round(left * ratio) + "px");
-                            var top = parseInt(jq_elem.css('top'), 10);
-                            jq_elem.css("top", Math.round(top * ratio) + "px");
+                    $(document).ready(function() {
+                        $(".navigation").off();
+                        $(".navigation").unbind();
+                        window.scrollMonitor = null;
+
+                        $(".topobox").each(function() {
+                            var tpbox = $(this);
+                            var canvas = tpbox.find("canvas");
+                            var canvas_width = canvas.width();
+
+                            tpbox.find(".nbr").each(function() {
+                                var jq_elem = $(this);
+                                jq_elem.data("orig-left", jq_elem.css('left'));
+                                jq_elem.data("orig-top", jq_elem.css('top'));
+                            });
+
+                            var updateNBRPositions = function() {
+                                var new_canvas_width = canvas.width();
+                                var ratio = new_canvas_width / canvas_width;
+                                tpbox.find(".nbr").each(function() {
+                                    var jq_elem = $(this);
+                                    var orig_left = parseInt(jq_elem.data('orig-left'), 10);
+                                    jq_elem.css("left", Math.round(orig_left * ratio) + "px");
+                                    var orig_top = parseInt(jq_elem.data('orig-top'), 10);
+                                    jq_elem.css("top", Math.round(orig_top * ratio) + "px");
+                                });
+                            };
+
+                            $(window).resize(updateNBRPositions);
                         });
-                        }, 0);
+
+                        $("head").append(" \
+                                <style type='text/css'> \
+                                @media screen and (max-width: 600px) { \
+                                    .topobox { \
+                                        width: 100% !important; \
+                                    } \
+                                    \
+                                    .topobox { \
+                                        height: auto !important; \
+                                    } \
+                                    \
+                                    .topobox img { \
+                                        width: 100% !important; \
+                                        height: auto !important; \
+                                    } \
+                                    \
+                                    .topobox canvas { \
+                                        width: 100%; \
+                                    } \
+                                } \
+                                </style> \
+                                ");
+
+                        $(".topobox").resize();
+
+                        $("table.crags th:last-child").after("<th>+</th>");
+                        $("table.crags tr").each(function() {
+                            var tr = $(this);
+                            var plus = $('<td><a href="#">+</a></td>').insertAfter($(this).find("td:last-child"));
+
+                            var plusHandler = function(evt) {
+                                var target  = $(evt.target);
+                                if( target.is('a') && target.attr("href") != "#") {
+                                    return true;
+                                }
+                                evt.preventDefault();
+                                plus.hide();
+                                var new_tr = $('<tr><td class="foldout" colspan="5"></td>').
+                                            insertAfter(tr);
+                                var new_td = new_tr.find("td");
+                                addCell(new_td, tr, 3, '<span class="stars"><div class="star full"></div></span>',
+                                                                                                       " &nbsp; ");
+                                addCell(new_td, tr, 4, "Routes:", " &nbsp; ");
+                                addCell(new_td, tr, 8, "Trad:", " &nbsp; ");
+                                addCell(new_td, tr, 10, "Topos:", " &nbsp; ");
+                                addCell(new_td, tr, 5, "under 5+:", " &nbsp; ");
+                                addCell(new_td, tr, 6, "6a-6c:", " &nbsp; ");
+                                addCell(new_td, tr, 6, "6c+-7b:", " &nbsp; ");
+                                addCell(new_td, tr, 6, "7b+ over:", "");
+
+                                var minus = $('<td><a href="#">-</a></td>').appendTo(tr);
+                                tr.off('click');
+
+                                tr.click(function(evt) {
+                                    var target  = $(evt.target);
+                                    if( target.is('a') && target.attr("href") != "#") {
+                                        return true;
+                                    }
+                                    evt.preventDefault();
+                                    plus.show();
+                                    new_tr.remove();
+                                    tr.click(plusHandler);
+                                    minus.remove();
+                                });
+                            };
+
+                            tr.click(plusHandler);
+                        });
                     });
                 </script>
                 </head>""")
