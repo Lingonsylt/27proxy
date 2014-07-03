6436b06307a24ae389ea28fd634e9e368b62e4d4
Anton Blomberg <anton@naetet.se>
2014-07-03 15:59:00 +0200

    Added options to choose address/port to bind to, as well as a DEBUG mode

--- 27proxy.py
+++ 27proxy.py
@@ -1,9 +1,19 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
 import requests
+import os
+
+PORT = int(os.environ.get("PROXY27_PORT", 8080))
+ADDRESS = os.environ.get("PROXY27_ADDRESS", '0.0.0.0')
+DEBUG = "PROXY27_DEBUG" in os.environ
+
+if not DEBUG:
+    with open('responsive.css') as f:
+        responsive_css = f.read()
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        global responsive_css
         r = requests.get("http://27crags.com%s" % self.path, headers=self.headers.dict)
         self.send_response(200)
         for header, value in r.headers.items():
@@ -15,8 +25,9 @@ class Handler(BaseHTTPRequestHandler):
             self.send_header("Content-Length", len(r.content))
             self.end_headers()
 
-            with open('responsive.css') as f:
-                responsive_css = f.read()
+            if DEBUG:
+                with open('responsive.css') as f:
+                    responsive_css = f.read()
             self.wfile.write(r.content.replace("</head>",
                                                "<meta name=\"viewport\" content=\"width=device-width\">\n" +
                                                "<style type=\"text/css\">\n%s\n</style>\n</head>" % responsive_css))
@@ -32,6 +43,7 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
 
 if __name__ == '__main__':
-    server = ThreadedHTTPServer(('0.0.0.0', 8080), Handler)
-    print 'Starting server, use <Ctrl-C> to stop'
+    server = ThreadedHTTPServer((ADDRESS, PORT), Handler)
+    print 'Starting server, use <Ctrl-C> to stop (mode: %s)' % ("DEBUG" if DEBUG else "PROD")
+    print "Listening to: %s:%s" % (ADDRESS, PORT)
     server.serve_forever()
